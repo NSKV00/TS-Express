d584f67aebbfbc5df042cab8e5126f6a2793ee4b
NICOLAS GIULIANI GARCIA DA SILVA <04852350051@senacrs.edu.br>
2025-07-09 20:22:45 -0300

    update createpost.service

--- src/services/createPost.service.ts
+++ src/services/createPost.service.ts
@@ -2,15 +2,24 @@ import { Repository } from "typeorm";
 import { CreatePost, Post, returnPostSchema } from "../schemas/posts.schemas";
 import { AppDataSource } from "../data-source";
 import { Posts } from "../entities/posts.entitie";
+import { Usuarios } from "../entities/usuarios.entitie";
 
-export const createPostService=async(postData:CreatePost):Promise<Post>=>{
+export const createPostService=async(postData:CreatePost,userId:number):Promise<Post>=>{
 
     const postRepository:Repository<Posts> = AppDataSource.getRepository(Posts)
+    const userRepository:Repository<Usuarios> = AppDataSource.getRepository(Usuarios)
 
-    const createPost  =postRepository.create(postData)
+    const findUser:Usuarios | null = await userRepository.findOne({
+        where:{
+            id: userId
+        }
+    })
+    const createPost:Post = postRepository.create({
+        ...postData,
+        usuario:findUser!
+    })
     await postRepository.save(createPost)
 
     const post = returnPostSchema.parse(createPost)
-
-    return post
+        return post
 }
